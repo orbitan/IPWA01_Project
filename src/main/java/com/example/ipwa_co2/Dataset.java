3ec624c1727e3dfcf9f57fc94e7c59bffa31a6ec
sarahh <sarah97hehn@outlook.de>
2022-03-27 00:49:59 +0100

    more code this time improved, but not beautified

--- src/main/java/com/example/ipwa_co2/Dataset.java
+++ src/main/java/com/example/ipwa_co2/Dataset.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 
 
-
 @ManagedBean
 public class Dataset {
     private final List<Country> countries = new ArrayList<>();
@@ -18,26 +17,40 @@ public class Dataset {
     public Dataset() {
         try {
             BufferedReader bf = new BufferedReader(new FileReader("/home/sarahh/Projects/IPWA01_Project/src/main/java/com/example/ipwa_co2/data.csv"));
-            String line;
 
+
+            String line;
             while ((line = bf.readLine()) != null) {
                 List<String> tempArr;
                 List<Double> finArr = new ArrayList<>();
                 tempArr = List.of(line.split(","));
 
-                for (int i = 1; i < tempArr.size(); i ++) {
-                    String n = tempArr.get(i);
-                    String t = n.replaceAll("^\"|\"$", "");
 
-                    try {
-                        Double k = Double.parseDouble(t);
-                        finArr.add(k);
-                    } catch (NumberFormatException exc) {
-                        finArr.add((double) 0);
+                try {
+//                        Double k = Double.parseDouble(n);
+//                        finArr.add(k);
+
+                    for(String s : tempArr.subList(1, tempArr.size()-2)){
+                        try {
+                            finArr.add(Double.parseDouble(s.replaceAll("^\"|\"$", "")));
+                            this.addToCountries(tempArr.get(0), finArr);
+                        } catch (NumberFormatException exc) {
+                            finArr.add((double) 0);
+                        }
+
                     }
+
+                } catch (NumberFormatException exc) {
+                    finArr.add((double) 0);
                 }
-                countries.add(new Country(tempArr.get(0).replaceAll("^\"|\"$", ""), finArr));
-                this.addToCountries(tempArr.get(0), finArr);
+
+//                for (int i = 1; i < tempArr.size(); i ++) {
+//                    String n = tempArr.get(i).replaceAll("^\"|\"$", "");
+//
+//                }
+
+
+
             }
 
         }catch (IOException ex) {
